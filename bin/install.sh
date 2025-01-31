f1fc604e50ab26f4570115f3c33427b6917c2cd0
Lusanco <93743011+Lusanco@users.noreply.github.com>
2025-01-30 22:36:50 -0400

    update - install script

---
+++ bin/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# bash 'strict' mode
+set -euo pipefall
+
+echo -e "\nInstalling dependencies..."
+
+npm create vite@latest . -- --template react-ts
+npm install tailwindcss @tailwindcss/vite
+npm i -D tailwindcss-motion
+npm install axios
+npm install @types/axios
+npm i react-router
+npm install @types/react-router
+npm install @reduxjs/toolkit
+
+echo "Dependencies installed."
