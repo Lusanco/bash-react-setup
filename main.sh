1056fa4e3a4aa2ed6e3b1b60b583d58209b90eff
Lusanco <93743011+Lusanco@users.noreply.github.com>
2025-01-30 23:13:54 -0400

    restructure - different approach 3 - final cleanup

--- main.sh
+++ main.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-# bash 'strict' mode
-set --euo pipefail
 
-# Load utility functions
-source "$(dirname "$0")/../utils/validate_directory.sh"
-
-# Ensure a directory is provided
-DIR="${1:-.}"
-validate_directory "$DIR"
-
-# Run setup scripts
-bash "$(dirname "$0")/setup.sh" "$DIR"
-echo -e "\nSetup complete! Run\n\n cd $DIR && npm run dev\n"
