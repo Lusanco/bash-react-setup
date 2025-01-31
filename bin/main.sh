e50f061b992a24acf5928df9e4cc6b357d4f89f2
Lusanco <93743011+Lusanco@users.noreply.github.com>
2025-01-30 22:27:27 -0400

    update - main file

---
+++ bin/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# bash 'strict' mode
+set --euo pipefall
+
+# Load utility functions
+source "$(dirname "$0")/../utils/validate_directory.sh"
+
+# Ensure a directory is provided
+DIR="${1:-.}"
+validate_directory "$DIR"
+
+# Run setup scripts
+bash "$(dirname "$0")/setup.sh" "$DIR"
+echo -e "\nSetup complete! Run\n\n cd $DIR && npm run dev\n"
